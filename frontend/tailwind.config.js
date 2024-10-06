206849b906945a50c11ec58805bacaf4432f67d2
Aditya <adityabasak7031@gmail.com>
2024-10-06 17:41:59 +0530

    add external css in the taileind config and indecx

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      //project colors
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E",
+      },
+    },
   },
   plugins: [],
 }
