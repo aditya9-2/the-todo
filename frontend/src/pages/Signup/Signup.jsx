96b5c264d8abd943abff7a0995c7ba386357362b
Aditya <adityabasak7031@gmail.com>
2024-10-07 18:44:21 +0530

    create signup page without BE Logic

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,9 +1,81 @@
-const Signup = () => {
-  return (
-    <div>
-      Signup
-    </div>
-  )
-}
+import { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Passwordinput from "../../components/input/Passwordinput";
+import { Link } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
 
-export default Signup
+const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setError("Please enter your name");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+  };
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="flex flex-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleSignup}>
+            <h4 className="text-2xl mb-7">Signup</h4>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input-box"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <input
+              type="text"
+              placeholder="EMail"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Passwordinput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+            <button type="submit" className="btn-primary">
+              create Account
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Already have an account?{" "}
+              <Link to="/login" className="font-medium text-primary">
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
