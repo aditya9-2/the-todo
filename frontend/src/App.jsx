982c246e99977fecf98f532b5ff502afff8c1232
Aditya <adityabasak7031@gmail.com>
2024-10-09 12:36:29 +0530

    replace Landing route to LandingPage route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Landing from "./pages/Landing/landing";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 const routes = (
   <Router>
     <Routes>
-      <Route path="/" exact element={<Landing />} />
+      <Route path="/" exact element={<LandingPage />} />
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<Signup />} />
