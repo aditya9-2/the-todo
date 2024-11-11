924a6627427d36a35377f1e9840e3e6f222aafd3
Aditya <adityabasak7031@gmail.com>
2024-11-11 20:24:53 +0530

    add  edit-note route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,11 +3,13 @@ import { createAccount } from "../controllers/userController.js";
 import { loginUser } from "../controllers/loginController.js";
 import addNote from "../controllers/addNoteController.js";
 import authenticationToken from "../middlewares/authenticationToken.js";
+import editNote from "../controllers/editNoteController.js";
 
 const router = express.Router();
 
 router.post('/create-account', createAccount);
 router.post('/login', loginUser);
 router.post('/add-note', authenticationToken, addNote);
+router.put('/edit-note/:noteId', authenticationToken, editNote);
 
 export default router;
