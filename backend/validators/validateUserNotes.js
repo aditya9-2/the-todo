857a5cbcdae9002016593d0e53bddad3aa003ab8
Aditya <adityabasak7031@gmail.com>
2024-11-11 19:43:36 +0530

    remove isPinned from zod validation

--- backend/validators/validateUserNotes.js
+++ backend/validators/validateUserNotes.js
@@ -3,7 +3,6 @@ import { z } from "zod";
 export const userNoteSchema = z.object({
     title: z.string().min(2, "Title is required"),
     content: z.string().min(2, "Content is required"),
-    isPinned: z.boolean()
 });
 
 export const validateUserNoteSchema = (data) => {
