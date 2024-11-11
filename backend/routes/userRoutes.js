bbe44527a2ad5d5cb165dda61c8d058050c91c16
Aditya <adityabasak7031@gmail.com>
2024-11-11 19:21:41 +0530

    add add-note route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { createAccount } from "../controllers/userController.js";
 import { loginUser } from "../controllers/loginController.js";
+import addNote from "../controllers/addNoteController.js";
+import authenticationToken from "../middlewares/authenticationToken.js";
 
 const router = express.Router();
 
 router.post('/create-account', createAccount);
 router.post('/login', loginUser);
+router.post('/add-note', authenticationToken, addNote);
 
 export default router;
