247a5b09bdf8b4edd9366b86d77ab14bd08a009c
Aditya <adityabasak7031@gmail.com>
2024-11-12 21:51:47 +0530

    add: moment

--- frontend/src/components/Cards/TodoCard.jsx
+++ frontend/src/components/Cards/TodoCard.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 
+import moment from "moment";
 import { MdOutlinePushPin, MdCreate, MdDelete } from "react-icons/md";
 
 const TodoCard = ({
@@ -16,7 +17,9 @@ const TodoCard = ({
       <div className="flex items-center justify-between">
         <div>
           <h6 className="text-sm font-medium">{title}</h6>
-          <span className="text-xs text-slate-500">{date}</span>
+          <span className="text-xs text-slate-500">
+            {moment(date).format("Do MMM YYYY")}
+          </span>
         </div>
 
         <MdOutlinePushPin
