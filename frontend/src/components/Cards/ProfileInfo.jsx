2ce0ca87402b37ef767017e72adb73a9be52d3dd
Aditya <adityabasak7031@gmail.com>
2024-11-12 18:54:31 +0530

    add useInfo from BE

--- frontend/src/components/Cards/ProfileInfo.jsx
+++ frontend/src/components/Cards/ProfileInfo.jsx
@@ -1,15 +1,17 @@
+/* eslint-disable react/prop-types */
 import { getInitialName } from "../../utils/helper";
 
-// eslint-disable-next-line react/prop-types
-const ProfileInfo = ({ onLogout }) => {
+const ProfileInfo = ({ userInfo, onLogout }) => {
+  if (!userInfo) return null;
+
   return (
     <div className="flex items-center gap-3">
       <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
-        {getInitialName("Aditya Basak")}
+        {getInitialName(userInfo.fullName)}
       </div>
 
       <div className="flex items-center gap-3">
-        <p className="text-sm font-medium">Aditya</p>
+        <p className="text-sm font-medium">{userInfo.fullName}</p>
         <button
           className="text-sm text-white bg-red-500 hover:bg-red-600 py-2 px-4 rounded cursor-pointer"
           onClick={onLogout}
