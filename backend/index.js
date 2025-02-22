e2439239fb34223c09de74f8c73c2ec36566dc5d
Aditya <adityabasak7031@gmail.com>
2025-02-23 00:07:59 +0530

    FE hosted in BE

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,28 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import userRouter from "./routes/userRoutes.js";
+import path from "path"
+import { fileURLToPath } from "url";
 
-dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cors({ origin: '*' }));
 
 app.use('/users', userRouter);
 
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 
 app.listen(port, async () => {
