e5df97811654e2c35b021b29703eedf6b7596a5f
Aditya <adityabasak7031@gmail.com>
2025-02-23 11:58:38 +0530

    rm dir

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,14 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import userRouter from "./routes/userRoutes.js";
-import path from "path"
-import { fileURLToPath } from "url";
+// import path from "path"
+// import { fileURLToPath } from "url";
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cors({ origin: '*' }));
@@ -19,11 +19,11 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/users', userRouter);
 
-app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+// app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
 
 
 app.listen(port, async () => {
